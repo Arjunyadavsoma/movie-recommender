b991d9fec874f842cdb5221baf616f10ce343d68
Arjunyadavsoma <arjunyadav35763@gmail.com>
2025-12-26 16:48:11 +0530

    Add trending section, movie details page, watchlist feature

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   images: {
     domains: ['image.tmdb.org'],
   },
-  env: {
-    NEXT_PUBLIC_TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY,
-  }
 }
 
 module.exports = nextConfig;
