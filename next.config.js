4f66242208d617f815c433350f5987401642694e
Arjunyadavsoma <arjunyadav35763@gmail.com>
2025-12-26 16:45:56 +0530

    Add trending section, movie details page, watchlist feature

--- next.config.js
+++ next.config.js
@@ -9,13 +9,4 @@ const nextConfig = {
   }
 }
 
-// next.config.js
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  }
-});
+module.exports = nextConfig;
